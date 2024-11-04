5be76cf87251539a49831591d4c7c4b8d5d36c86
Joshua Tan <joshuatanx@gmail.com>
2024-11-05 00:00:20 +0100

    set up initial signup page

--- josh/script.js
+++ josh/script.js
@@ -1,3 +1,58 @@
+var lazyImages = document.querySelectorAll("img.wardrobe.tops");
+
+
+const options = {
+    root: document.getElementById("carousel__tops"),
+    rootMargin: "0px",
+    threshold: 1.0,
+};
+
+let lazyImageObserver = new IntersectionObserver(handleIntersection, options);
+
+function handleIntersection(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio >= 0.2) {
+			entry.target.src = "assets/images/DIANA.webp"
+            console.log("VISIBLE");
+        }
+    });
+}
+
+console.log(lazyImages)
+
+lazyImages.forEach(function(lazyImage) {
+	lazyImageObserver.observe(lazyImage);
+});
+
+/* let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+        }
+    });
+});
+
+lazyImages.forEach(function(lazyImage) {
+    lazyImageObserver.observe(lazyImage);
+});
+ */
+
+
+
+
+/* const options = {
+    root: document.getElementById("heading__profession-list"),
+    rootMargin: "0px",
+    threshold: 1.0,
+}; */
+
+//const observer = new IntersectionObserver(handleIntersection, options);
+
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
