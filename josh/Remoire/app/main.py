91198d008671a293fcc4c6b4da76b766e8febbd0
Joshua Tan <joshuatanx@gmail.com>
2024-11-10 12:55:51 +0100

    initialize react

--- josh/Remoire/app/main.py
+++ josh/Remoire/app/main.py
@@ -5,7 +5,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @main.route('/home')
 @login_required
--- josh/script.js
+++ josh/script.js
@@ -1,5 +1,9 @@
 var lazyImages = document.querySelectorAll("img.wardrobe.tops");
+var carouselTops = document.querySelectorAll("div#carousel__tops");
+console.log(carouselTops[0].children);
 
+let array = Array.from(carouselTops)[0].children;
+/* console.log(array.shift()) */
 
 const options = {
     root: document.getElementById("carousel__tops"),
@@ -7,22 +11,58 @@ const options = {
     threshold: 1.0,
 };
 
-let lazyImageObserver = new IntersectionObserver(handleIntersection, options);
+let lazyImageObserver = new IntersectionObserver(handleLazyImageIntersection, options);
 
-function handleIntersection(entries, observer) {
+function handleLazyImageIntersection(entries, observer) {
     entries.forEach((entry) => {
-        if (entry.intersectionRatio >= 0.2) {
-			entry.target.src = "assets/images/DIANA.webp"
-            console.log("VISIBLE");
+        if (entry.isIntersecting) {
+            const div = document.createElement("div");
+            div.classList.add("card");
+
+            const img = document.createElement("img");
+            img.classList.add("wardrobe", "tops", "lazy");
+            img.setAttribute("src", "assets/images/DIANA.webp");
+
+            div.appendChild(img);
+            carouselTops[0].appendChild(div);
+            lazyImageObserver.unobserve(entry.target);
+            
+            console.log(array);
+            array[0].remove();
+            array = Array.from(carouselTops)[0].children;
+            carouselTops[0].scroll(0, -1 * array[0].getBoundingClientRect().width);
+            console.log(array[0].offsetWidth);
+
+            console.log(entry.target);
+            lazyImageObserver.observe(array[array.length - 2]);
+            // the problem is that when the first element is removed, it affects the scroll position 
+            // of the entire carousel which keeps the observed element on screen
+
+            /* 
+            
+            lazyImageObserver.unobserve(entry);
+            entry.setAttribute("--observed", "false");
+            lazyImageObserver.observe(carouselTops[carouselTops.length - 5]);
+            carouselTops[carouselTops.length - 5].setAttribute("--observed", "true"); */
         }
     });
 }
 
-console.log(lazyImages)
 
-lazyImages.forEach(function(lazyImage) {
+
+lazyImageObserver.observe(array[array.length - 2]);
+
+
+
+
+/* console.log(lazyImages)
+console.log(lazyImages[lazyImages.length - 5]); */
+/* lazyImageObserver.observe(lazyImages[lazyImages.length - 5]); // observe 4th last image
+carouselTops[carouselTops.length - 5].setAttribute("--observed", "true"); */
+
+/* lazyImages.forEach(function(lazyImage) {
 	lazyImageObserver.observe(lazyImage);
-});
+}); */
 
 /* let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
     entries.forEach(function(entry) {
