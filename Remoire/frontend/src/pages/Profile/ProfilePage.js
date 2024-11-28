81164523e1ded3b71ceeae5d834fa64be21d4bf0
Joshua Tan <joshuatanx@gmail.com>
2024-11-28 11:56:22 +0100

    update profile page user verification

--- Remoire/frontend/src/pages/Profile/ProfilePage.js
+++ Remoire/frontend/src/pages/Profile/ProfilePage.js
@@ -6,10 +6,18 @@ import { useContext } from "react";
 export default function ProfilePage() {
     const { user, setUser } = useContext(UserContext);
 
+    if (user && user !== -1) {
+        return (
+            <>
+                <Header />
+                <h1>{user.username}</h1>
+            </>
+        );
+    }
     return (
         <>
             <Header />
-            {user ? <h1>{user.username}'s profile</h1> : <h1>Profile</h1>}
+            {user && user !== -1 ? <h1>{user.username}</h1> : <h1>Profile</h1>}
         </>
     );
 }
