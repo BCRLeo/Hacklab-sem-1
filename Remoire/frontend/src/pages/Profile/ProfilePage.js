20042225876babe41d07dcb0d7dba4a56a719bb7
Joshua Tan <joshuatanx@gmail.com>
2024-11-28 16:33:12 +0100

    feat(profile): add user posts to profile

--- Remoire/frontend/src/pages/Profile/ProfilePage.js
+++ Remoire/frontend/src/pages/Profile/ProfilePage.js
@@ -2,12 +2,56 @@ import "./ProfilePage.css";
 
 import Header from "../../components/Header/Header";
 import Icon from "../../components/Icon/Icon";
+import Post from "../../components/Post/Post";
 
 import { UserContext } from "../../UserContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function ProfilePage() {
     const { user, setUser } = useContext(UserContext);
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        try {
+            const response = await fetch("/api/posts", {
+                method: "GET"
+            });
+    
+            if (response.ok) {
+                const postsMetadata = await response.json();
+                
+                const postComponents = await Promise.all(postsMetadata.map(async (postMetadata) => {
+                    try {
+                        const imageResponse = await fetch(postMetadata.url);
+                        const imageBlob = await imageResponse.blob();
+                        const imageUrl = URL.createObjectURL(imageBlob);
+    
+                        return (
+                            <Post
+                                key={postMetadata.id}
+                                postId={postMetadata.id}
+                                image={imageUrl}
+                                username={postMetadata.username}
+                            />
+                        );
+                    } catch (imageError) {
+                        console.error(`Error fetching image for post ${postMetadata.id}:`, imageError);
+                        return null;
+                    }
+                }));
+    
+                setPosts(postComponents.filter(Boolean));
+            } else {
+                console.error("Failed to fetch posts");
+            }
+        } catch (error) {
+            console.error("Error fetching posts:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, [user]);
 
     if (user && user !== -1) {
         return (
@@ -15,6 +59,8 @@ export default function ProfilePage() {
                 <Header />
                 <Icon className="profile-icon" name="accountIcon" size="xl" />
                 <h1>{user.username}</h1>
+                <p>bio test test i'm so cool test test fashion whatever</p>
+                {posts}
             </>
         );
     }
