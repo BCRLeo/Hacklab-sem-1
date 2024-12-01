14c482adddd217794596c485145854805ba46cd1
BCRLeo <leonardo.jean.cremona@gmail.com>
2024-12-01 11:00:35 +0100

    added back the bakcgounrd removal to see if better resource helps

--- Remoire/app/main.py
+++ Remoire/app/main.py
@@ -116,7 +116,7 @@ def upload_wardrobe_item():
         
     # Read the image data and get the MIME type
     file_data = file.read()
-    #file_data = ImageBackgroundRemoverV1.remove_background_file(file_data)
+    file_data = ImageBackgroundRemoverV1.remove_background_file(file_data)
     mimetype = file.mimetype
 
     if not mimetype.startswith('image/'):
