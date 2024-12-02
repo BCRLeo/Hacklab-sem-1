92c41796587a70a4204017b1adb62b4cf2395d80
Joshua Tan <joshuatanx@gmail.com>
2024-12-02 11:29:13 +0100

    update routing and navbar based on if user is logged in

--- Remoire/frontend/src/App.js
+++ Remoire/frontend/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
-import MediaQueryProvider from './MediaQueryContext';
+import React, { useState, useEffect, useContext } from "react";
 import Clothes from './components/Clothes/Clothes';
 import Header from './components/Header/Header';
 import Outfits from './components/Outfits/Outfits';
@@ -15,55 +14,34 @@ import SignUpPage from "./pages/SignUp/SignUpPage"
 import WardrobePage from './pages/Wardrobe/WardrobePage';
 
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { UserProvider } from './UserContext';
+import { UserContext } from './UserContext';
 
 export default function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [username, setUsername] = useState("");
-	const [user, setUser] = useState({"isLoggedIn": isLoggedIn, "username": username});
-
-	// Fetch login status from Flask
-	useEffect(() => {
-		fetch('/api/check-login')
-		.then((response) => response.json())
-		.then((data) => {
-			if (data.success) {
-				setIsLoggedIn(true);
-				setUsername(data.user.username);
-				setUser(data.user);
-			}
-		})
-		.catch((error) => {
-			console.error('Error fetching login status:', error);
-		});
-	}, []);
+	const { user } = useContext(UserContext);
 
 	return (
-		<MediaQueryProvider>
-			<UserProvider>
-				<Router>
-					<Header />
-					<Routes>
-						<Route path="feed" element={<FeedPage />} />
-						<Route index element={<HomePage />} />
-						<Route path="home" element={<HomePage />} />
-						<Route path="login" element={<LogInPage />} />
-						<Route path=":username" element={<ProfilePage />}>
-							<Route index element={<Navigate to="posts" replace />} />
-							<Route path="posts" element={<Posts />} />
-							<Route path="clothes" element={<Clothes />} />
-							<Route path="outfits" element={<Outfits />} />
-						</Route>
-						<Route path="signup" element={<SignUpPage />} />
-						<Route path="wardrobe" element={<WardrobePage />}>
-							<Route index element={<Navigate to="clothes" replace />} />
-							<Route path="clothes" element={<Clothes />} />
-							<Route path="outfits" element={<Outfits />} />
-						</Route>
-						<Route path="search" element={<SearchPage />} />
-					</Routes>
-				</Router>
-			</UserProvider>
-		</MediaQueryProvider>
+		<Router>
+			<Header />
+			<Routes>
+				<Route index element={user && user !== -1 ? <Navigate to="feed" replace /> : <HomePage />} />
+				<Route path="feed" element={<FeedPage />} />
+				<Route path="login" element={<LogInPage />} />
+				<Route path="signup" element={<SignUpPage />} />
+				<Route path=":username" element={<ProfilePage />}>
+					<Route index element={<Navigate to="posts" replace />} />
+					<Route path="posts" element={<Posts />} />
+					<Route path="clothes" element={<Clothes />} />
+					<Route path="outfits" element={<Outfits />} />
+				</Route>
+				<Route path="wardrobe" element={<WardrobePage />}>
+					<Route index element={<Navigate to="clothes" replace />} />
+					<Route path="clothes" element={<Clothes />} />
+					<Route path="outfits" element={<Outfits />} />
+				</Route>
+				<Route path="search" element={<SearchPage />} />
+				{/* Catch-all route to redirect to feed */}
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		</Router>
 	);
 }
--- Remoire/frontend/src/index.js
+++ Remoire/frontend/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { UserProvider } from './UserContext';
+import MediaQueryProvider from './MediaQueryContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<MediaQueryProvider>
+			<UserProvider>
+				<App />
+			</UserProvider>
+		</MediaQueryProvider>
 	</React.StrictMode>
 );
